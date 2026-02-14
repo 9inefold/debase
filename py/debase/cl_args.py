5aa60c334d708583e568ced88a99f0f289d3a264
eightfold <97198162+9inefold@users.noreply.github.com>
2026-02-14 02:19:59 -0500

    Update debase_library

---
+++ py/debase/cl_args.py
@@ -0,0 +1,62 @@
+import os, argparse
+from pathlib import Path
+
+def parse_args():
+  parser = argparse.ArgumentParser(prog="debase")
+  parser.add_argument(
+    '--version', action='version',
+    version="1.0.0"
+  )
+
+  parser.add_argument(
+    '--build-type',
+    dest='build_type',
+    type=str,
+    choices=['Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel'],
+    help='the build type'
+  )
+  parser.add_argument(
+    '--frame-pointer',
+    dest='frame_pointer',
+    type=str, default='all',
+    choices=['all', 'non-leaf', 'none'],
+    help='--frame-pointer=VALUE passed to LLC on Release'
+  )
+  parser.add_argument(
+    '--target', '--target-name',
+    dest='target',
+    type=str,
+    help='the target file'
+  )
+  parser.add_argument(
+    '-o', '--output-dir',
+    dest='output',
+    type=Path, default=os.getcwd(),
+    help='output directory'
+  )
+  parser.add_argument(
+    '--output-filename', '--output-filenames',
+    dest='jsonout',
+    type=str,
+    help='arguments passed to debase'
+  )
+  parser.add_argument(
+    '--passthrough',
+    dest='passthrough',
+    type=str, default='',
+    help='arguments passed to debase'
+  )
+  parser.add_argument(
+    dest='files',
+    action='extend',
+    nargs='*',
+    type=str, default=[],
+    help='[filenames...]'
+  )
+
+  args = parser.parse_args()
+  files = []
+  for f in args.files:
+    files.extend(f.split(';'))
+  args.files = files
+  return args
--- py/debase/main.py
+++ py/debase/main.py
@@ -1,15 +1,104 @@
-import os, sys
-from subprocess import run as run_process
+import os, sys, json
+from subprocess import run as exec_process
+from debase.cl_args import parse_args
+from pathlib import Path
 
 def errs(*args, **kwargs):
   print(*args, file=sys.stderr, **kwargs)
 
-def debase_main(args, debase_bin, llc_bin):
-  #if not '-o' in args:
-  #  errs('-o is required for debasing!')
-  #  sys.exit(0)
-  if not '--output-filenames' in args:
-    args.append('--output-filenames')
+def run_process(args, _cwd):
+  if type(args) is list:
+    args = ' '.join(args)
+  return exec_process(args, cwd=_cwd, capture_output=True, text=True)
+
+def run_debaser(debase_bin, args):
+  o = Path(args.output)
+  target = Path(args.target)
+  jsonout = args.jsonout
+  passthrough = args.passthrough.split(';')
+  passthrough.extend(['--allow-no-builtins', '--permissive'])
+
+  if not (target / o).exists():
+    errs('target', target.as_posix(), 'does not exist!')
+    sys.exit(1)
+
+  (o / 'lib').mkdir(parents=True, exist_ok=True)
+
+  debase_args = [
+    debase_bin,
+    (o / target).as_posix(),
+    '-o', (o / 'lib').as_posix(),
+    f'--output-filenames={jsonout}',
+    '--verbose'
+  ]
+  debase_args.extend(passthrough)
+  debase_args.extend(args.files)
+  print(args.files)
+  #print(debase_args)
+
+  result = run_process(debase_args, str(o))
+  if result.returncode != 0:
+    errs('failed to run debaser!')
+    #errs(result.stdout.strip())
+    errs(result.stderr.strip())
+    sys.exit(result.returncode)
   
-  #sys.exit(1)
-  pass
+  return (o / 'lib' / jsonout)
+
+def run_llc(llc_bin, args, bc_files: list[str]):
+  o = Path(args.output) / 'opt'
+  o.mkdir(exist_ok=True)
+
+  llc_O = {
+    'Debug': ['-O=0', '--frame-pointer=none'],
+    'RelWithDebInfo': ['-O=2', '--frame-pointer=non-leaf'],
+    'MinSizeRel': ['-O=2', f'--frame-pointer={args.frame_pointer}'],
+    'Release': ['-O=3', f'--frame-pointer={args.frame_pointer}'],
+  }
+
+  out_files = []
+  failed = False
+
+  #llc -O=3 --regalloc=pbqp -filetype=obj -o ./Class.o lib/Class.ll
+  for bc_file in bc_files:
+    out = o / (Path(bc_file).stem + '.o')
+    llc_args = [
+      llc_bin,
+      *llc_O[args.build_type],
+      '--regalloc=pbqp', '-filetype=obj',
+      '-o', out.as_posix(),
+      bc_file
+    ]
+
+    #print(llc_args)
+    result = run_process(llc_args, o.as_posix())
+    if result.returncode == 0:
+      out_files.append(out.as_posix())
+    else:
+      errs('failed to run llc on', Path(bc_file).as_posix(), '!')
+      #errs(result.stdout.strip())
+      errs(result.stderr.strip())
+      failed = True
+  
+  if failed:
+    sys.exit(1)
+  return out_files
+
+def debase_main(debase_bin, llc_bin):
+  args = parse_args()
+  jsonout = run_debaser(debase_bin, args)
+  json_data = json.loads(jsonout.read_text())
+
+  if json_data['files'] is None:
+    errs('invalid debase json output: could not find "files": [...]')
+    sys.exit(1)
+  
+  bc_files = json_data['files']
+  if type(bc_files) is str:
+    bc_files = [bc_files]
+  
+  out_files = run_llc(llc_bin, args, bc_files)
+  print(out_files)
+
+  sys.exit(1)
+  (o / 'file.txt').write_text('hello!')
